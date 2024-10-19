41ab7befee62510970bd6b783707693a4ba1ed27
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-19 20:06:38 +0600

    grid

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,13 @@
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags, population} = country
+    const {name, flags, population, area} = country
     console.log(country)
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="" /> 
-            <h3>Population: {population}</h3>
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
         </div>
     );
 };
